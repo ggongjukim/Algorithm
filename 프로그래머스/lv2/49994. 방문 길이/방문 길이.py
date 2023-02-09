639ad23e3839bc8507eb090bbdde5aad87a6cd48
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-02-09 22:44:59 +0900

    [level 2] Title: 방문 길이, Time: 1.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -16,17 +16,13 @@ def solution(dirs):
         if not(end[0] >= -5 and end[0]<=5 and end[1] >=-5 and end[1] <=5):
             continue
         
-        # print("now", num+1,str(end) in road,way[-1],end,road)
-#         #지나간 길인지 확인 start -> end
+        #지나간 길인지 확인 start -> end
         if str(end) in road and way[-1] in road[str(end)]: # 지나간 점이고 지나간 길이면
-            # print("\n지나간길",road, "\n")
             way.append(end)
             continue            
         
-#         #반대쪽도 지나간 길인지 확인
-        # print("now", num+1,str(way[-1]) in road,way[-1],end,road)
+        #반대쪽도 지나간 길인지 확인
         if str(way[-1]) in road and end in road[str(way[-1])]:
-            # print("지나간길")
             way.append(end)
             continue
         
@@ -37,9 +33,7 @@ def solution(dirs):
         except: road[str(end)] = [way[-1]]
         way.append(end)
         
-    # print(num+1,"way",way,"\n",road)
     answer = road.values()
-    # print("answer",answer)
     count = 0
     for i in answer:
         count += len(i)

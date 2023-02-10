e6714025550cad863cb4a55f25191cdb29efdc3f
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-02-10 23:58:57 +0900

    [level 3] Title: 기지국 설치, Time: 3.09 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,17 @@
+import math
+def solution(n, stations, w):
+    answer = 0
+    s = 0 
+    ran = w*2+1
+    for i in stations:
+        
+        e = (i-1)-(w)-1
+        # print(s,e)
+        answer += math.ceil((e-s+1)/ran)
+        s = (i-1)+w+1
+    
+    # 끝이 처리가 안되어있는 경우
+    if not (n-1 >= stations[-1] - 1 -w and n-1 <= stations[-1] -1 +w):
+        e = n-1
+        answer +=  math.ceil((e-s+1)/ran)
+    return answer

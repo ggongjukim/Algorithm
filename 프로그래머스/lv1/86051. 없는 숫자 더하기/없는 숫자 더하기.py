3c7acce5274fa3878cbb2665b1a05479e636f3c5
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-02-16 16:07:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+# 문제 0부터 9까지 숫자중 일부가 들어있는 정수배열 numbers
+# numbers에서 찾을 수 없는 숫자를 모두 찾아 더ㅓ한수를 return
+def solution(numbers):
+    answer = -1
+    # sum = 56
+    numbersSum = sum(numbers)
+    return 45 - numbersSum
+
+
+
+# solution = lambda x: sum(range(10)) - sum(x)

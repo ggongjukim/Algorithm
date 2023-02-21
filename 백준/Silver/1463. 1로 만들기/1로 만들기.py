0ab9bb62814827954e28e0cccd2f7193c8e583f9
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-02-22 02:48:05 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 39504 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,10 +1,10 @@
 N = int(input())
 dp =[0,0]
 
-for i in range(2,N+1):
-    dp.append(dp[i-1]+1)
+for i in range(2,N+1): 
+    dp.append(dp[i-1]+1) # 일단 -1 연산 한거 넣어주고
     if i%2== 0: 
-        dp[i] = min([dp[i], dp[i//2]+1])
+        dp[i] = min([dp[i], dp[i//2]+1]) # 연산 -1 한거랑 비교 
     if i%3 == 0 : 
-        dp[i] = min([dp[i],dp[i//3]+1])
+        dp[i] = min([dp[i],dp[i//3]+1]) # 6의 배수라도 앞에서 2로 나눈 연산값과 최소 비교를 하기 때문에 상관없다 단, elif 로 하면 안된다
 print(dp[N])

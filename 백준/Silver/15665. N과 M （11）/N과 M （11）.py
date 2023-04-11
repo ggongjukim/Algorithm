b481f9ddf292daea79d931ef4cab49a5740c1ec6
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-04-11 22:28:43 +0900

    [Silver II] Title: N과 M (11), Time: 1036 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,28 @@
+"""
+같은 애를 다시뽑을 수 있음 => 중복
+1 9 랑 9 1 다름 => 순열
+"""
+n, m = map(int, input().split())
+arr = list(map(int,input().split()))
+arr = list(set(arr))
+arr = sorted(arr)
+# print(arr)
+answer = []
+def printanswer():
+  # print(visited)
+  for i in range(len(answer)):
+      print(answer[i], end=' ')
+  print()
+  # print(temp)
+  
+def sol(cnt):
+  if cnt == m+1:
+    printanswer()
+    return
+
+  for i in range(len(arr)):
+    answer.append(arr[i])
+    sol(cnt+1)
+    answer.pop()
+    
+sol(1)

cfe9f4bdfed8a5fd171c6d333a8fedf62cffc755
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-04-11 20:29:06 +0900

    [Silver III] Title: N과 M (8), Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,27 @@
+"""
+중복 가능
+순열
+backtracking(cur_num)
+"""
+n, m = map(int, input().split())
+arr = list(map(int,input().split()))
+arr = sorted(arr)
+answer = []
+def printvisited():
+  for i in answer:
+      print(i,end=' ')
+  print()
+
+def sol(cur_num):
+  if cur_num == m+1 :
+    printvisited()
+    return
+
+  for i in range(n):
+    if cur_num > 1 and answer[-1] > arr[i]:
+      continue
+    answer.append(arr[i])
+    sol(cur_num+1)
+    answer.pop()
+
+sol(1)

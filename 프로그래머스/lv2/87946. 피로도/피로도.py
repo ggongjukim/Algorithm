4ce8152982557d1e5728c90eb9b7440662db97e3
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-04-13 18:31:54 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,25 @@
+def sol(cnt,dungeons,k):
+    global answer
+#     if cnt == len(dungeons):
+#         answer = max(answer, cnt)
+#         return
+    if cnt > answer:
+        answer = cnt
+        
+    for i in range(len(dungeons)):
+        if k < dungeons[i][0]:
+            continue
+        if visited[i]:
+            continue
+        visited[i] = 1
+        sol(cnt+1, dungeons,k - dungeons[i][1])
+        visited[i] = 0
+    
+answer = 0
+visited= []
+
+def solution(k, dungeons):
+    global visited
+    visited = [False] * len(dungeons)
+    sol(0,dungeons,k)
+    return answer

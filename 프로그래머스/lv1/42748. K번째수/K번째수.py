0197a6155a63b0377ea47e932c3e6c3668ab6961
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-04-16 14:19:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,12 +1,8 @@
 def solution(array, commands):
   answer = []
 
-  for x in commands:
-    i = int(x[0])-1
-    j = int(x[1])
-    k = int(x[2])-1
-    print(i,j,k)
-    temp = array[i:j]
+  for i,j,k in commands:
+    temp = array[i-1:j]
     temp.sort()
-    answer.append(temp[k])
+    answer.append(temp[k-1])
   return answer

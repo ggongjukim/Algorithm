6122bd09a6eef2476290f3d39a02a7411ed4224e
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-04-20 11:35:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 2056 ms, Memory: 212288 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,63 @@
+"""
+첫줄 수 개수
+둘줄 수 배열
+셋줄 사칙연산 개수
+사칙연산에 대해 
+"""
+n = int(input())
+num_arr = list(map(int,input().split()))
+cal = list(map(int,input().split()))
+cal_arr = []
+max_num = 1000000000
+min_num = -1000000000
+answer =[]
+for i in range(4):
+  if i == 0:
+    temp = '+'
+  elif i == 1:
+    temp = '-'
+  elif i == 2:
+    temp = '*'
+  elif i == 3:
+    temp = '//'
+  cal_arr = cal_arr + [temp] * cal[i]
+
+# print(cal_arr)
+visited = [False] * len(cal_arr)
+
+def print_cal():
+  temp = num_arr[0]
+  for i in range(1,len(num_arr)):
+    if cal_ans[i-1] == "+":
+      temp = temp + num_arr[i]
+    elif cal_ans[i-1] == "-":
+      temp = temp - num_arr[i]
+    elif cal_ans[i-1] == "*":
+      temp = temp * num_arr[i]
+    elif cal_ans[i-1] == "//":
+      if temp < 0 :
+        temp *= -1
+        temp = temp // num_arr[i]
+        temp *= -1
+      else:
+        temp = temp // num_arr[i]
+  answer.append(temp)
+  return
+  
+cal_ans =[]
+def sol(cur_num):
+  if cur_num == len(cal_arr):
+    print_cal()
+    return
+  for i in range(len(cal_arr)):
+    if visited[i]:
+      continue
+    cal_ans.append(cal_arr[i])
+    visited[i] = True
+    sol(cur_num+1)
+    cal_ans.pop()
+    visited[i] = False
+sol(0)
+
+print(max(answer))
+print(min(answer))

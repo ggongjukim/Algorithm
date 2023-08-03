b2d74aa673780a8ece530b790dc9e97326040d7a
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-08-03 20:31:36 +0900

    [Silver III] Title: N과 M (2), Time: 136 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,30 +1,19 @@
-"""
-n 이 3이면 1 2 3 중에 고르기
-visited [f f f]
-중복 안되고
-오름차순
-m 개의 개수만 뽑아야함
-조합 순서 상관없음
-"""
-n,m = map(int,input().split())
-visited = [False] * (n)
+N,M = list(map(int,input().split()))
+arr =[]
 
-def sol(cnt,last_idx):
-  # cnt 개 수를  뽑았고, 마지막 숫자 idx는  last_idx
-  if cnt == m:
-    # print(visited)
-    for i in range(n):
-      if visited[i]:
-        print(i+1,end= ' ')
+def sol(num):
+  if num == M:
+    for i in arr:
+      print(i,end=' ')
     print()
     return
 
-  for i in range(last_idx+1, n):
-    visited[i] = True
-    sol(cnt+1,i)
-    visited[i] = False
+  for i in range(1,N+1):
+    if len(arr) >0:
+      if arr[-1] >= i:
+        continue
+    arr.append(i)
+    sol(num+1)
+    arr.pop()
 
-for i in range(n):
-  visited[i] = True
-  sol(1, i)
-  visited[i] = False
+sol(0)

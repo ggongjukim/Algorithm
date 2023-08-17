71525cabacd096d19f70a2402d04b75abf882475
ggongjukim <75241542+ggongjukim@users.noreply.github.com>
2023-08-17 21:43:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,28 @@
+def solution(s):
+  result = len(s)
+  minlen = len(s)
+  # print('len(s)',len(s))
+  for k in range(1,len(s)//2+1): # 2부터 절반까지 
+    arr = []
+    total = 1
+    count = 0
+    compare = s[0:k]
+    finish=''
+
+
+    for i in range(0+k, len(s)+k,k):
+      # print(k,i,compare)
+      if compare == s[i:i+k]:
+        total += 1
+      else :
+        if total == 1:
+          finish += compare
+        else:
+          finish += str(total)+compare
+        compare = s[i:i+k]
+        total = 1
+
+    result = len(finish)
+    if minlen > result:
+      minlen = result
+  return minlen
